48d50052cd093197278b2202eafd9f631e31b2f3
Roshan Chaudhari <98824802+roshanchaudhari7@users.noreply.github.com>
2024-08-29 22:27:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+	let boldWords = document.querySelectorAll('strong');
+	for(let t of boldWords) {
+		t.style.color = "green";
+	}
 }
 
 
 function return_normal() {
     //Write your code here
-
+	let boldWords = document.querySelectorAll('strong');
+	for(let t of boldWords) {
+		t.style.color = "black";
+	}
     
 }
